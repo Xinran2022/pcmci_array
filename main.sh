9c1194775b5a4a82ed2f590dc5ae90318e244f51
Ricardo Barros Lourenco <ricardobarroslourenco@gmail.com>
2023-10-20 17:19:29 -0400

    Debugging routine

--- main.sh
+++ main.sh
@@ -25,8 +25,11 @@ indices=$(awk -v offset=$header_offset -F, 'NR > 1 {print $1 + offset}' paramete
 # Load python 3.11.5 (please customize given your workload - perhaps with a python venv)
 module load StdEnv/2023 python/3.11.5
 
+# Adjust SLURM_ARRAY_TASK_ID to account for the header line
+adjusted_task_id=$((SLURM_ARRAY_TASK_ID + header_offset))
+
 # Extracting parameters using sed and awk
-line=$(sed -n "${SLURM_ARRAY_TASK_ID}p" parameters.csv)
+line=$(sed -n "${adjusted_task_id}p" parameters.csv)
 index=$(echo $line | awk -F, '{print $1}')
 temperature=$(echo $line | awk -F, '{print $2}')
 category=$(echo $line | awk -F, '{print $3}')
