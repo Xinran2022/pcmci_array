53a4ad9a5a6fb51ada09c59cf4f011b6813fddf0
Ricardo Barros Lourenco <ricardobarroslourenco@gmail.com>
2023-10-20 16:55:27 -0400

    Debugging routine

--- main.sh
+++ main.sh
@@ -23,18 +23,7 @@ indices=$(cut -d, -f1 parameters.csv | sort -n | tr '\n' ',' | sed 's/,$//')
 module load StdEnv/2023 python/3.11.5
 
 # Fetch the corresponding csv line based on the index value
-csv_line=$(awk -v var=$SLURM_ARRAY_TASK_ID -F, '$1==var {print $0}' parameters.csv)
-
-# Read the csv line into an array
-IFS=, read -a line <<< "$csv_line"
-
-# Debugging echo
-echo "Debugging - Index: ${line[0]}, Temperature: ${line[1]}, Category: ${line[2]}" >> debug.txt
-
-# Extracting parameters (columns are the parameters)
-index=${line[0]}
-temperature=${line[1]}
-category=${line[2]}
+eval $(awk -v var=$SLURM_ARRAY_TASK_ID -F, '$1==var {printf "index=%d temperature=%.1f category=%s", $1, $2, $3}' parameters.csv)
 
 # Running the python script with the extracted parameters
 python main.py --index "$index" --temperature "$temperature" --category "$category"
