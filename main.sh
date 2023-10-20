14cc2c994d2f108d12be7369c8545808b9b2d240
Ricardo Barros Lourenco <ricardobarroslourenco@gmail.com>
2023-10-20 17:48:04 -0400

    Working? (let´s flood my e-mail)

--- main.sh
+++ main.sh
@@ -2,16 +2,21 @@
 
 # The indices list is then consumed by the slurm --array argument,
 # which for every task, gets into a Slurm Array Task ID
-# The --time flag, is max time per task (default below 3h to get higher priority)
+
+# The time/mem/cpus flag, is max time per task (default below 3h to get higher priority)
+
+# The array 2-1000 corresponds to the index 2,3,4,...,1000 range, contained on parameters.csv
 
 #SBATCH --job-name=ja_ex
-#SBATCH --output=out_%j.txt
-#SBATCH --error=err_%j.txt
-#SBATCH --array=2-4
+#SBATCH --output=./slurm_runs/%j-%u-%x-%N.out
+#SBATCH --error=./slurm_runs/%j-%u-%x-%N.err
+#SBATCH --array=2-1000
 #SBATCH --time=00:30:00
 #SBATCH --account=def-ggalex
 #SBATCH --mem-per-cpu=2G   # 2 GB of memory per CPU
 #SBATCH --cpus-per-task=4  # 4 CPUs per task
+#SBATCH --mail-user=barroslr@mcmaster.ca
+#SBATCH --mail-type=ALL
 
 # Set the csv header size (1 contains only a first line, often containing column titles)
 header_offset=1
@@ -27,10 +32,6 @@ line=$(sed -n "${adjusted_task_id}p" parameters.csv)
 index=$(echo $line | awk -F, '{print $1}')
 temperature=$(echo $line | awk -F, '{print $2}')
 category=$(echo $line | awk -F, '{print $3}')
-
-# Debugging echo
-echo "Debugging - Index: $index, Temperature: $temperature, Category: $category" >> debug.txt
-echo "This task ID#: $SLURM_ARRAY_TASK_ID" >> debug.txt
 
 # Running the python script with the extracted parameters
 python main.py --index "$index" --temperature "$temperature" --category "$category"
---
+++ slurm_runs/about.md
@@ -0,0 +1,3 @@
+# About slurm_runs
+
+This folder contains the runs of slurm calls. This file needs to be kept as a placeholder.
